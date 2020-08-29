2d61100f84ea97575c2dee9aeb50efbaf39a2903
Dewanshu Rahul4 <dewanshurahul95@gmail.com>
2020-08-29 10:25:48 +0530

    Adding: Checking Valid E-mail Id

--- user_registraton.py
+++ user_registraton.py
@@ -8,14 +8,27 @@ def first_caps_three_letter(first_name):
     return False
 
 
+def valid_email(mail):
+    email = "[a-zA-Z0-9]+@[a-zA-Z]+\.(com|org|net)"
+    if re.search(email, mail):
+        return True
+    return False
+
+
 firstname = input("Enter first Name : ")
 if first_caps_three_letter(firstname):
-    print(firstname)
+    print("First Name is", firstname)
 else:
     print("First Name not in correct format")
 
 lastname = input("Enter Last Name : ")
 if first_caps_three_letter(lastname):
-    print(lastname)
+    print("Last Name is", lastname)
 else:
     print("Last Name not in correct format")
+
+email_id = input("Enter your E-mail : ")
+if valid_email(email_id):
+    print("Email_id is", email_id)
+else:
+    print("Invalid E-mail ID")
