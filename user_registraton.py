06631f002182bcdab66b190cbf271f121a66051a
Dewanshu Rahul4 <dewanshurahul95@gmail.com>
2020-08-29 09:57:14 +0530

    Adding; First_Name with caps and Atleast three letter in it

---
+++ user_registraton.py
@@ -0,0 +1,15 @@
+import re
+
+
+def first_caps_three_letter(first_name):
+    count = '^.{3,}$'
+    if re.match("^[A-Z]", first_name) and re.match(count, first_name):
+        return True
+    return False
+
+
+firstname = input("Enter first Name")
+if first_caps_three_letter(firstname):
+    print(firstname)
+else:
+    print("First Name not in correct format")
