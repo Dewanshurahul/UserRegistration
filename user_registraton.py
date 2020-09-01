c8e5dffae7010df94bcccb6d03b4f7507c4f3d9c
Dewanshu Rahul4 <dewanshurahul95@gmail.com>
2020-09-01 07:57:44 +0530

    Refactor: Adding Switch Case for different Validations

--- user_registraton.py
+++ user_registraton.py
@@ -41,20 +41,35 @@ if first_caps_three_letter(lastname):
 else:
     print("Last Name not in correct format")
 
-email_id = input("Enter your E-mail : ")
-if valid_email(email_id):
-    print("Email_id is", email_id)
-else:
-    print("Invalid E-mail ID")
+# email_id = input("Enter your E-mail : ")
+# if valid_email(email_id):
+#     print("Email_id is", email_id)
+# else:
+#     print("Invalid E-mail ID")
+#
+# mobile_number = input("Enter Mobile Number as XX-XXXXXXXXXX")
+# if mobile_format(mobile_number):
+#     print("Mobile Number is", mobile_number)
+# else:
+#     print("Invalid Mobile Number Format")
+#
+# password = input("Enter Password : ")
+# if valid_password(password):
+#     print("Passowrd is", password)
+# else:
+#     print("Enter atleast 8 digit Password with atleast one Numeric digit and Upper Case in it")
 
-mobile_number = input("Enter Mobile Number as XX-XXXXXXXXXX")
-if mobile_format(mobile_number):
-    print("Mobile Number is", mobile_number)
-else:
-    print("Invalid Mobile Number Format")
 
-password = input("Enter Password : ")
-if valid_password(password):
-    print("Passowrd is", password)
-else:
-    print("Enter atleast 8 digit Password with atleast one Numeric digit and Upper Case in it")
+def select_validation(case):
+    if case == 1:
+        print(valid_email(input("Enter your E-mail: ")))
+    elif case == 2:
+        print(mobile_format(input("Enter Mobile Number as XX-XXXXXXXXXX: ")))
+    elif case == 3:
+        print(valid_password(input("Enter Password : ")))
+    else:
+        print("Invalid Input Enter value between 1-3",)
+
+
+choice = int(input("Enter 1: for e-Mail Validation\nEnter 2: for Mobile Number Validation\nEnter 3: for Password Validation: "))
+select_validation(choice)
